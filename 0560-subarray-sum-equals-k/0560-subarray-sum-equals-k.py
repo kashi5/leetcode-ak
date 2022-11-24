409551bcf70dbb5b60089803665a791b28aaa74d
kashi5 <kashi560@gmail.com>
2022-11-24 18:21:13 +0530

    Time: 398 ms (77.08%), Space: 16.6 MB (75.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,5 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # hash map solution
         sum_ = 0
         count = 0
         dict_ = {0:1}
@@ -10,6 +11,24 @@ class Solution:
                 count+=dict_.get(target, 0)
             dict_[sum_]=dict_.get(sum_, 0) +1
         return count
+        
+#         using prefixsum time O(n2) TLE for python
+#         count = 0
+#         prefix_sum_list =[0]
+#         prefix_sum = 0
+#         for i in nums:
+#             prefix_sum += i
+#             prefix_sum_list.append(prefix_sum)
+#         print(prefix_sum_list)
+#         for x in range(len(nums)):
+#             num_1 = prefix_sum_list[x]
+#             for y in range(x+1,len(nums)+1):
+#                 num_2 = prefix_sum_list[y]
+#                 if k == num_2 - num_1:
+#                     count += 1
+#         return count
+            
+            
             
                 
         
