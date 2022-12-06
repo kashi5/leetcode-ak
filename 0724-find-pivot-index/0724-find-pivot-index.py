821231a1e1903504776a558387ba27946e16dbc2
kashi5 <kashi560@gmail.com>
2022-12-06 20:18:45 +0530

    Time: 412 ms (25.74%), Space: 15.3 MB (11.43%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -3,6 +3,7 @@ class Solution:
         S= sum(nums)
         leftsum = 0
         for i, x in enumerate(nums):
+            m = S -leftsum - x
             if leftsum == (S -leftsum - x):
                 return i
             leftsum += x
