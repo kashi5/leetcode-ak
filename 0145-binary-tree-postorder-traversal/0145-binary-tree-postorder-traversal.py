8852bf08250bc1172d3f2adfc605cd75ef0ac6a8
kashi5 <kashi560@gmail.com>
2023-02-03 07:48:35 -0800

    Time: 28 ms (88.15%), Space: 13.9 MB (9.65%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -4,19 +4,64 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        array=[]
-        self.pTraversal(root,array)
-        return array
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         # array=[]
+#         # self.pTraversal(root,array)
+#         # return array
+#         # 2,3,1
+        
+#         if root is None:
+#             return []
+        
+#         stack, output = [root, ], []
+#         while stack:
+#             # root = stack.pop()
+#             # if root is not None:
+#             #     output.append(root.val)
+#             #     if root.right is not None:
+#             #         stack.append(root.right)
+#             #     if root.left is not None:
+#             #         stack.append(root.left)
+#             if root.right is not None:
+#                 stack.append(root.right)
+#             if root.left is not None:
+#                 stack.append(root.left)
+#             root = stack.pop()
+                
+        
+#         return output
     
         
-    def pTraversal(self,root,array):
-        if root is not None:
-            self.pTraversal(root.left,array)
-            self.pTraversal(root.right,array)
-            array.append(root.val)
-        return array
+    # def pTraversal(self,root,array):
+    #     if root is not None:
+    #         self.pTraversal(root.left,array)
+    #         self.pTraversal(root.right,array)
+    #         array.append(root.val)
+    #     return array
                 
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        stack, output = [], []
+        while root or stack:
+            # push nodes: right -> node -> left 
+            while root:
+                if root.right:
+                    stack.append(root.right)
+                stack.append(root)
+                root = root.left
+            
+            root = stack.pop()
+            
+            # if the right subtree is not yet processed
+            if stack and root.right == stack[-1]:
+                stack[-1] = root
+                root = root.right
+            # if we're on the leftmost leaf
+            else:
+                output.append(root.val)
+                root = None
+                
+        return output
                 
                 
