91478d1616d01cf8aefaec80f18707dde03edc90
kashi5 <kashi560@gmail.com>
2023-08-12 12:10:07 +0530

    Time: 1 ms (91.23%), Space: 44 MB (94.57%) - LeetHub

--- 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.java
+++ 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.java
@@ -14,17 +14,18 @@
  * }
  */
 class Solution {
+    //Approach #1 (Top Down Depth-first Search)
     private int maxLength = 0;
-public int longestConsecutive(TreeNode root) {
-    dfs(root, null, 0);
-    return maxLength;
-}
+    public int longestConsecutive(TreeNode root) {
+        dfs(root, null, 0);
+        return maxLength;
+    }
 
-private void dfs(TreeNode p, TreeNode parent, int length) {
-    if (p == null) return;
-    length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
-    maxLength = Math.max(maxLength, length);
-    dfs(p.left, p, length);
-    dfs(p.right, p, length);
-}
+    private void dfs(TreeNode p, TreeNode parent, int length) {
+        if (p == null) return;
+        length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
+        maxLength = Math.max(maxLength, length);
+        dfs(p.left, p, length);
+        dfs(p.right, p, length);
+    }
 }
